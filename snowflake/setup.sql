660cb29296bf9a3d378d8231ab0ba4df12463dcd
YESHWANTH KUMAR THALAPANENI <152114012+yeshthalapaneni@users.noreply.github.com>
2026-04-21 08:06:00 -0400

    Create database and schema for streaming data

---
+++ snowflake/setup.sql
@@ -0,0 +1,5 @@
+CREATE OR REPLACE DATABASE realtime_streaming_db;
+CREATE OR REPLACE SCHEMA realtime_streaming_db.streaming_schema;
+
+USE DATABASE realtime_streaming_db;
+USE SCHEMA streaming_schema;
