8301acae0e5cebe3741da93a6f3349153deff3a7
YESHWANTH KUMAR THALAPANENI <152114012+yeshthalapaneni@users.noreply.github.com>
2026-04-21 08:07:14 -0400

    Add STORAGE_INTEGRATION to raw_events_stage

--- snowflake/external_stage.sql
+++ snowflake/external_stage.sql
@@ -4,3 +4,20 @@ USE SCHEMA streaming_schema;
 CREATE OR REPLACE STAGE raw_events_stage
 URL = 's3://your-raw-bucket-name/'
 FILE_FORMAT = json_file_format;
+
+
+
+
+
+
+
+
+
+
+USE DATABASE realtime_streaming_db;
+USE SCHEMA streaming_schema;
+
+CREATE OR REPLACE STAGE raw_events_stage
+URL = 's3://your-raw-bucket-name/'
+STORAGE_INTEGRATION = your_storage_integration_name
+FILE_FORMAT = json_file_format;
